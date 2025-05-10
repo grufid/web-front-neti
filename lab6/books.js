9640f195d0a5c49f53006c44f4986d1cfc6ff475
Shamshieva Dayana <shamshievad05@gmail.com>
2025-05-10 19:17:01 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,98 @@
+const books = [
+    {
+        author: 'Чынгыз Айтматов',
+        name: 'Джамиля',
+        publicationYear: 1958,
+        price: '480₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'Вокруг света за 80 дней',
+        publicationYear: 1872,
+        price: '284₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'Властелин Мира',
+        publicationYear: 1904,
+        price: '450₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'Двадцать тысяч лье под водой',
+        publicationYear: 1870,
+        price: '717₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Жюль Верн',
+        name:'Путешествие к центру земли',
+        publicationYear: 1864,
+        price: '459₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'Таинственный остров',
+        publicationYear: 1875,
+        price: '330₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'Дети капитана Гранта',
+        publicationYear: 1867,
+        price: '303₽',
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'С Земли на Луну прямым путем за 97 часов 20 минут ',
+        publicationYear: 1865,
+        price: '480₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'Плавучий остров',
+        publicationYear: 1895,
+        price: '480₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'Маяк на краю света',
+        publicationYear: 1905,
+        price: '300₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'Пятнадцатилетний капитан',
+        publicationYear: 1878,
+        price: '579₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Жюль Верн',
+        name: 'Вокруг Луны',
+        publicationYear: 1869,
+        price: '580₽',
+        bestseller: false,
+        available: false
+    }
+];
